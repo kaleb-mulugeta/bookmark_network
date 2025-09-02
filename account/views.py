784045a139cab4c9163103472771fef0f327f2e8
kaleb-mulugeta <skalebsmulu@gmail.com>
2025-09-02 16:16:29 +0300

    profile editing done

--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+
 
 # Create your views here.
 def user_login(request):
@@ -58,4 +60,56 @@ def register(request):
         request,
         'account/register.html',
         {'user_form': user_form}
+    )
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
+    )
+
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance = request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form' : user_form,
+            'profile_form' : profile_form
+        }
     )
