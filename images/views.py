e4532f47b51bc904d32f5741b28ac9b68ea23f9f
kaleb-mulugeta <skalebsmulu@gmail.com>
2025-09-16 08:51:30 +0300

    Storing image views in Redis

--- images/views.py
+++ images/views.py
@@ -11,6 +11,14 @@ from django.views.decorators.http import require_POST
 from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
 from django.http import HttpResponse
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+r = redis.Redis(
+    host = settings.REDIS_HOST,
+    port = settings.REDIS_PORT,
+    db = settings.REDIS_DB 
+)
 
 @login_required
 def image_create(request):
@@ -39,12 +47,14 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
     return render(
         request,
         'images/image/detail.html',
         {
             'section': 'images',
-            'image': image
+            'image': image,
+            'total_views': total_views
         }
     )
 
